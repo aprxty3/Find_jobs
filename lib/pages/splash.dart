f69ef0ba250bf85e6de31c9c2ebb2834d53f1786
aprxrt3 <ajiprxty3@gmail.com>
2021-11-22 21:20:26 +0700

    add State Timer splash screen to onboarding page

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:async';
+import 'onboardingPage.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -7,6 +9,16 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => OnboardingPage()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
