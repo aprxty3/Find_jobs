381c41e984dea7b61165063aad9e74bdb4c2cb32
aprxrt3 <ajiprxty3@gmail.com>
2021-11-23 10:46:05 +0700

    bug fix

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,59 +1,37 @@
-import 'package:find_job/pages/startedPage.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'dart:async';
 
-class SplashScreen extends StatefulWidget   {
+import 'package:find_job/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return StartState();
-  }
+  _SplashPageState createState() => _SplashPageState();
 }
 
-class StartState extends State<SplashScreen> {
+class _SplashPageState extends State<SplashScreen> {
+  @override
   void initState() {
     super.initState();
-    startTimer();
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        '/onboarding',
+        (route) => false,
+      );
+    });
   }
-}
 
-startTimer() async {
-  var duration = Duration(seconds: 5);
-  return Timer(duration, route);
-}
-
-route() {
-  BuildContext context;
-  Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context) => GetStartedPage()));
-}
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Color(0xff4141A4),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: Image.asset(
-              'assets/logo.png',
-              width: 59,
-              height: 76,
-            ),
-          ),
-          SizedBox(height: 50),
-          Text(
-            'FIND JOBS',
-            style: GoogleFonts.poppins(
-              fontWeight: FontWeight.w600,
-              fontSize: 32,
-              color: Colors.white,
-            ),
-          ),
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Image.asset(
+          'assets/image_splash.png',
+          width: 178,
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
