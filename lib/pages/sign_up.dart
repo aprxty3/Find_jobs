92ffe572bbbf3913554591faf5f0bd6ec80eec86
aprxrt3 <ajiprxty3@gmail.com>
2021-11-30 10:53:44 +0700

    add singlechildview and navigator to sign in

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,3 +1,4 @@
+import 'package:find_job/pages/sign_in.dart';
 import 'package:find_job/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
@@ -16,150 +17,192 @@ class _SignUpPageState extends State<SignUpPage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(
-            top: 20,
-            left: 24,
-            right: 24,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                ' Sign Up ',
-                style: tittleStyle,
-              ),
-              Text(
-                'Begin New Journey',
-                style: subtittleStyle,
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Center(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'assets/icon_upload.png',
-                      width: 120,
-                      height: 120,
-                    ),
-                    SizedBox(height: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Fullname',
-                          style: tittleStyle,
-                        ),
-                        SizedBox(height: 8),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            fillColor: Color(0xffF1F0F5),
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide.none,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide(color: Color(0xff4141A4)),
-                            ),
-                            hintText: '',
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          'Email Address',
-                          style: tittleStyle,
-                        ),
-                        SizedBox(height: 8),
-                        TextFormField(
-                          controller: emailController,
-                          onChanged: (value) {
-                            print(value);
-                            bool isValid = EmailValidator.validate(value);
-                            print(isValid);
-                            if (isValid) {
-                              setState(() {
-                                isEmailValid = true;
-                              });
-                            } else {
-                              setState(() {
-                                isEmailValid = false;
-                              });
-                            }
-                          },
-                          decoration: InputDecoration(
-                            fillColor: Color(0xffF1F0F5),
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide.none,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide(
-                                  color: isEmailValid
-                                      ? Color(0xff4141A4)
-                                      : Color(0xffFD4F56)),
-                            ),
-                            hintText: '',
-                          ),
-                          style: TextStyle(
-                              color: isEmailValid
-                                  ? Color(0xff4141A4)
-                                  : Color(0xffFD4F56)),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          'Password',
-                          style: tittleStyle,
-                        ),
-                        SizedBox(height: 8),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            fillColor: Color(0xffF1F0F5),
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide.none,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide(color: Color(0xff4141A4)),
-                            ),
-                            hintText: '',
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          'Goal',
-                          style: tittleStyle,
-                        ),
-                        SizedBox(height: 8),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            fillColor: Color(0xffF1F0F5),
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide.none,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(100),
-                              borderSide: BorderSide(color: Color(0xff4141A4)),
-                            ),
-                            hintText: '',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 24,
+              right: 24,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  ' Sign Up ',
+                  style: tittleStyle,
                 ),
-              ),
-            ],
+                Text(
+                  'Begin New Journey',
+                  style: subtittleStyle,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/icon_upload.png',
+                        width: 120,
+                        height: 120,
+                      ),
+                      SizedBox(height: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Fullname',
+                            style: tittleStyle,
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              fillColor: Color(0xffF1F0F5),
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                    BorderSide(color: Color(0xff4141A4)),
+                              ),
+                              hintText: '',
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Email Address',
+                            style: tittleStyle,
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            controller: emailController,
+                            onChanged: (value) {
+                              print(value);
+                              bool isValid = EmailValidator.validate(value);
+                              print(isValid);
+                              if (isValid) {
+                                setState(() {
+                                  isEmailValid = true;
+                                });
+                              } else {
+                                setState(() {
+                                  isEmailValid = false;
+                                });
+                              }
+                            },
+                            decoration: InputDecoration(
+                              fillColor: Color(0xffF1F0F5),
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide: BorderSide(
+                                    color: isEmailValid
+                                        ? Color(0xff4141A4)
+                                        : Color(0xffFD4F56)),
+                              ),
+                              hintText: '',
+                            ),
+                            style: TextStyle(
+                                color: isEmailValid
+                                    ? Color(0xff4141A4)
+                                    : Color(0xffFD4F56)),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Password',
+                            style: tittleStyle,
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              fillColor: Color(0xffF1F0F5),
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                    BorderSide(color: Color(0xff4141A4)),
+                              ),
+                              hintText: '',
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Goal',
+                            style: tittleStyle,
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              fillColor: Color(0xffF1F0F5),
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                    BorderSide(color: Color(0xff4141A4)),
+                              ),
+                              hintText: '',
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 30),
+                      Container(
+                        width: 400,
+                        height: 40,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            backgroundColor: Color(0xff4141A4),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(66),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Sign Up',
+                            style: buttonStyle,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 30),
+                        child: Center(
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SignIn(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Back To Sign In',
+                              style: button2Style,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
