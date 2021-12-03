6c2bccee9426d54a649b14c28225036b09986bec
aprxrt3 <ajiprxty3@gmail.com>
2021-12-03 11:13:11 +0700

    add image body

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:find_job/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class homePage extends StatelessWidget {
   @override
@@ -33,7 +34,7 @@ class homePage extends StatelessWidget {
                   'assets/image_profile.png',
                   width: 58,
                   height: 58,
-                )
+                ),
               ],
             ),
           ),
@@ -41,10 +42,58 @@ class homePage extends StatelessWidget {
       );
     }
 
+    Widget body() {
+      return Container(
+        child: Padding(
+          padding: EdgeInsets.only(left: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hot Categories',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  color: Color(0xff272C2F),
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                width: 150,
+                height: 200,
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 16, bottom: 16),
+                    child: Text(
+                      'Website Developer',
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/image_category1.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           header(),
+          SizedBox(height: 30),
+          body(),
         ],
       ),
     );
