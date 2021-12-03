b2461213f59b02976bc09a4094cac0c5b9427cf6
aprxrt3 <ajiprxty3@gmail.com>
2021-12-03 19:50:18 +0700

    rename

---
+++ lib/pages/apply_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class applyPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
