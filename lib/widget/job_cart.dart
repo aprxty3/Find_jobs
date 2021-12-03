dacbe91cf600e3af6334c945e2be1832ec78a55f
aprxrt3 <ajiprxty3@gmail.com>
2021-12-03 15:44:44 +0700

    add navigator to detailed page

--- lib/widget/job_cart.dart
+++ lib/widget/job_cart.dart
@@ -1,3 +1,4 @@
+import 'package:find_job/pages/detailed_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,29 +10,38 @@ class jobCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      height: 200,
-      child: Align(
-        alignment: Alignment.bottomLeft,
-        child: Padding(
-          padding: EdgeInsets.only(left: 16, bottom: 16),
-          child: Text(
-            text,
-            style: GoogleFonts.poppins(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => detailePage(),
+          ),
+        );
+      },
+      child: Container(
+        width: 150,
+        height: 200,
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 16, bottom: 16),
+            child: Text(
+              text,
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
         ),
-      ),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(imageUrl),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(imageUrl),
+          ),
         ),
       ),
     );
-    
   }
 }
