2796af6377f6f0823999b775ce84a7969ed793ba
aprxrt3 <ajiprxty3@gmail.com>
2021-12-03 12:50:16 +0700

    create widget

---
+++ lib/widget/job_cart.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class jobCart extends StatelessWidget {
+  final String text;
+  final String imageUrl;
+
+  jobCart({this.imageUrl, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 200,
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Padding(
+          padding: EdgeInsets.only(left: 16, bottom: 16),
+          child: Text(
+            text,
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(imageUrl),
+        ),
+      ),
+    );
+    
+  }
+}
