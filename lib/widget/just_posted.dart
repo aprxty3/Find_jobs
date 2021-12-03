9e7c7cd4d337c3effb0a008d92e6547a64c6d796
aprxrt3 <ajiprxty3@gmail.com>
2021-12-03 20:07:41 +0700

    add navigator to apply page

--- lib/widget/just_posted.dart
+++ lib/widget/just_posted.dart
@@ -1,3 +1,4 @@
+import 'package:find_job/pages/apply_page.dart';
 import 'package:find_job/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -11,32 +12,42 @@ class justPosted extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            imageURL,
-            width: 44,
-            height: 45,
-          ),
-          SizedBox(width: 27),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(jobText, style: jobStyle),
-                Text(comText, style: jobStyle2),
-                SizedBox(
-                  height: 18,
-                ),
-                Divider(
-                  color: Color(0xffECEDF1),
-                  thickness: 1,
-                )
-              ],
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => applyPage(),
             ),
-          )
-        ],
+          );
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              imageURL,
+              width: 44,
+              height: 45,
+            ),
+            SizedBox(width: 27),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(jobText, style: jobStyle),
+                  Text(comText, style: jobStyle2),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  Divider(
+                    color: Color(0xffECEDF1),
+                    thickness: 1,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
