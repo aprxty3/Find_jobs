083792c4777b6b74518da21763cb337f50ff2037
aprxrt3 <ajiprxty3@gmail.com>
2021-12-03 20:07:57 +0700

    created apply page and header

--- lib/pages/apply_page.dart
+++ lib/pages/apply_page.dart
@@ -1,12 +1,34 @@
+import 'package:find_job/theme.dart';
 import 'package:flutter/material.dart';
 
 class applyPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 80),
+              child: Image.asset(
+                'assets/icon_google.png',
+                width: 60,
+                height: 60,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Front-End Developer',
+              style: applyTheme1,
+            ),
+            SizedBox(height: 2),
+            Text(
+              'Google, Inc • Jakarta',
+              style: applyTheme2,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
