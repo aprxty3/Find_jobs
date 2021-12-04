5d52d6bbf5c0860b90b10043b0464239368166bb
aprxrt3 <ajiprxty3@gmail.com>
2021-12-04 11:02:31 +0700

    add state provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,35 @@ import 'package:find_job/pages/sign_in.dart';
 import 'package:find_job/pages/sign_up.dart';
 import 'package:find_job/pages/splash.dart';
 import 'package:find_job/pages/started_page.dart';
+import 'package:find_job/provider/auth_provider.dart';
+import 'package:find_job/provider/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/onboarding': (context) => GetStartedPage(),
-        '/sign-in': (context) => SignIn(),
-        '/sign-up': (context) => SignUpPage(),
-        '/home': (context) => homePage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (context) => AuthProvider(),
+        ),
+        ChangeNotifierProvider<UserProvider>(
+          create: (context) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => SplashScreen(),
+          '/onboarding': (context) => GetStartedPage(),
+          '/sign-in': (context) => SignIn(),
+          '/sign-up': (context) => SignUpPage(),
+          '/home': (context) => homePage(),
+        },
+      ),
     );
   }
 }
