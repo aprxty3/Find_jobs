fbcaa17394f4f9e1b144136b05f5b3f721c58466
aprxrt3 <ajiprxty3@gmail.com>
2021-12-07 11:57:12 +0700

    add provider and setstate API

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -1,7 +1,11 @@
+import 'package:find_job/model/user_model.dart';
 import 'package:find_job/pages/sign_up.dart';
+import 'package:find_job/provider/auth_provider.dart';
+import 'package:find_job/provider/user_provider.dart';
 import 'package:find_job/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:provider/provider.dart';
 
 import 'homePage.dart';
 
@@ -14,8 +18,22 @@ class _SignInState extends State<SignIn> {
   TextEditingController emailController = TextEditingController(text: '');
   TextEditingController passwordController = TextEditingController(text: '');
 
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
+    var authProvider = Provider.of<AuthProvider>(context);
+    var userProvider = Provider.of<UserProvider>(context);
+
+    void showError(String message) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: redColor,
+          content: Text(message),
+        ),
+      );
+    }
+
     Widget header() {
       return Container(
         margin: EdgeInsets.only(top: 30),
@@ -145,27 +163,51 @@ class _SignInState extends State<SignIn> {
         height: 45,
         width: double.infinity,
         margin: EdgeInsets.only(top: 40),
-        child: TextButton(
-          onPressed: () async {
-            Navigator.pushNamedAndRemoveUntil(
-              context,
-              '/home',
-              (route) => false,
-            );
-          },
-          style: TextButton.styleFrom(
-            backgroundColor: primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(66),
-            ),
-          ),
-          child: Text(
-            'Sign In',
-            style: whiteTextStyle.copyWith(
-              fontWeight: medium,
-            ),
-          ),
-        ),
+        child: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : TextButton(
+                onPressed: () async {
+                  if (emailController.text.isEmpty ||
+                      passwordController.text.isEmpty) {
+                    showError('Semua kolom harap diisi');
+                  } else {
+                    setState(() {
+                      isLoading = true;
+                    });
+
+                    userModel user = await authProvider.login(
+                      emailController.text,
+                      passwordController.text,
+                    );
+
+                    setState(() {
+                      isLoading = false;
+                    });
+
+                    if (user == null) {
+                      showError('email atau password salah');
+                    } else {
+                      userProvider.user = user;
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, '/home', (route) => false);
+                    }
+                  }
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(66),
+                  ),
+                ),
+                child: Text(
+                  'Sign In',
+                  style: whiteTextStyle.copyWith(
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
       );
     }
 
