58df4c1c3de092a7b9bfb485ba0c8a7c10024412
aprxrt3 <ajiprxty3@gmail.com>
2021-12-07 12:55:01 +0700

    add provider

---
+++ lib/provider/cate_provider.dart
@@ -0,0 +1,9 @@
+import 'package:find_job/model/category_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CateProvider with ChangeNotifier {
+  Future<List<CateModel>> getCate() async{
+    
+  }
+}
