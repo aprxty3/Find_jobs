c6bc705ef0044a0cf281d73e30383b4a65179885
aprxrt3 <ajiprxty3@gmail.com>
2021-12-08 13:58:44 +0700

    integration API

--- lib/pages/apply_page.dart
+++ lib/pages/apply_page.dart
@@ -1,264 +1,276 @@
+import 'package:find_job/model/job_model.dart';
 import 'package:find_job/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class applyPage extends StatefulWidget {
+  final JobModel job;
+  applyPage(this.job);
+
   @override
   State<applyPage> createState() => _applyPageState();
 }
 
 class _applyPageState extends State<applyPage> {
-  bool isAplied = false;
+  bool isApplied = false;
+
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 80,
+        ),
+        child: Column(
+          children: [
+            isApplied
+                ? Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: 30),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(48),
+                      color: Color(0xffECEDF1),
+                    ),
+                    child: Text(
+                      'You have applied this job and the\nrecruiter will contact you',
+                      textAlign: TextAlign.center,
+                      style: greyTextStyle.copyWith(),
+                    ),
+                  )
+                : Container(),
+            Image.network(
+              widget.job.companyLogo,
+              width: 60,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              widget.job.name,
+              style: blackTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text(
+              '${widget.job.companyName} • ${widget.job.location}',
+              style: greyTextStyle,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget detailItem(String text) {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 16,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.adjust,
+              color: primaryColor,
+              size: 12,
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Text(
+                text,
+                style: blackTextStyle.copyWith(
+                  fontWeight: light,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget aboutJob() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'About the job',
+              style: blackTextStyle.copyWith(
+                fontWeight: medium,
+              ),
+            ),
+            Column(
+              children: widget.job.about
+                  .map(
+                    (text) => detailItem(text),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget qualifications() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Qualifications',
+              style: blackTextStyle.copyWith(
+                fontWeight: medium,
+              ),
+            ),
+            Column(
+              children: widget.job.qualifications
+                  .map(
+                    (text) => detailItem(text),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget responsibilities() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Responsibilities',
+              style: blackTextStyle.copyWith(
+                fontWeight: medium,
+              ),
+            ),
+            Column(
+              children: widget.job.responsibilities
+                  .map(
+                    (text) => detailItem(text),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget applyButton() {
-      return Container(
-        width: 200,
-        height: 45,
-        child: TextButton(
-          style: TextButton.styleFrom(
-            backgroundColor: Color(0xff4141A4),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(66),
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 50),
+            height: 45,
+            width: 200,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  isApplied = true;
+                });
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(66),
+                ),
+              ),
+              child: Text(
+                'Apply for Job',
+                style: whiteTextStyle.copyWith(
+                  fontWeight: medium,
+                ),
+              ),
             ),
           ),
-          onPressed: () {
-            setState(() {
-              isAplied = !isAplied;
-            });
-          },
-          child: Text(
-            'Apply for Job',
-            style: buttonStyle,
-          ),
-        ),
+        ],
       );
     }
 
-    Widget cancelapplyButton() {
-      return Container(
-        width: 200,
-        height: 45,
-        child: TextButton(
-          style: TextButton.styleFrom(
-            backgroundColor: Color(0xffFD4F56),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(66),
+    Widget cancelButton() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 50),
+            height: 45,
+            width: 200,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  isApplied = false;
+                });
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: redColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(66),
+                ),
+              ),
+              child: Text(
+                'Cancel Apply',
+                style: whiteTextStyle.copyWith(
+                  fontWeight: medium,
+                ),
+              ),
             ),
           ),
-          onPressed: () {
-            setState(() {
-              isAplied = !isAplied;
-            });
-          },
-          child: Text(
-            'Cancel Apply',
-            style: buttonStyle,
-          ),
-        ),
+        ],
       );
     }
 
-    Widget cancelapplyfly() {
+    Widget messageButton() {
       return Container(
-        margin: EdgeInsets.only(bottom: 30),
-        padding: EdgeInsets.fromLTRB(25, 10, 25, 10),
-        decoration: BoxDecoration(
-          color: Color(0xffECEDF1),
-          borderRadius: BorderRadius.circular(50),
+        margin: EdgeInsets.only(
+          top: 20,
+          bottom: 35,
         ),
-        child: Text(
-          'You have applied this job and the \nrecruiter will contact you',
-          textAlign: TextAlign.center,
-          style: GoogleFonts.poppins(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-            color: Color(0xffA2A6B4),
+        child: TextButton(
+          onPressed: () {},
+          child: Text(
+            'Message Recruiter',
+            style: greyTextStyle.copyWith(
+              fontWeight: medium,
+            ),
           ),
         ),
       );
     }
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 80),
-              isAplied ? cancelapplyfly() : SizedBox(),
-              Image.asset(
-                'assets/icon_google.png',
-                width: 60,
-                height: 60,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Front-End Developer',
-                style: applyTheme1,
-              ),
-              SizedBox(height: 2),
-              Text(
-                'Google, Inc • Jakarta',
-                style: applyTheme2,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 30.0, left: 24),
-                child: Column(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'About the job',
-                          style: applyTheme3,
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Full-Time On Site',
-                              style: applyTheme4,
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Start at \$18,000 per month',
-                              style: applyTheme4,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 30),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Qualification',
-                          style: applyTheme3,
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Candidate must possess at least a \nBachelor\'s Degree.',
-                              style: applyTheme4,
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Able to use Microsoft Office and Google \nbased service.',
-                              style: applyTheme4,
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Have an excellent time management, \ngood at organized and details',
-                              style: applyTheme4,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 30),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Responbilities',
-                          style: applyTheme3,
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Initiate and promote any programs, events, \ntraining, or activities.',
-                              style: applyTheme4,
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Image.asset(
-                              'assets/dot.png',
-                              width: 12,
-                              height: 12,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Assessing and anticipating needs and \ncollaborate with Division.',
-                              style: applyTheme4,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 30),
-              isAplied ? cancelapplyButton() : applyButton(),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 20,
-                  bottom: 30,
-                ),
-                child: InkWell(
-                  onTap: () {},
-                  child: Text(
-                    'Message Recruiter',
-                    style: button2Style,
-                  ),
-                ),
-              ),
-            ],
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
           ),
+          children: [
+            header(),
+            aboutJob(),
+            qualifications(),
+            responsibilities(),
+            isApplied ? cancelButton() : applyButton(),
+            messageButton(),
+          ],
         ),
       ),
     );
